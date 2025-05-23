580f694612a4706b3a4073af65b8478d3ec1ff06
hadiuzzaman909 <hadiuzzaman909@outlook.com>
2025-05-23 18:41:05 +0600

    added facebook login

--- vellora-angular/src/app/core/config/app.config.ts
+++ vellora-angular/src/app/core/config/app.config.ts
@@ -3,12 +3,26 @@ import { provideRouter } from '@angular/router';
 import { routes } from '../routes/app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations'; 
+import { SocialLoginModule, SocialAuthServiceConfig, FacebookLoginProvider } from '@abacritt/angularx-social-login';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideClientHydration(withEventReplay()),
-    provideAnimations()
+    provideAnimations(),
+    SocialLoginModule,
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('558665937009709'),
+          },
+        ],
+      } as SocialAuthServiceConfig,
+    },
   ]
 };
--- vellora-angular/src/app/feature/customer/home/home.component.ts
+++ vellora-angular/src/app/feature/customer/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { BannerComponent } from '../banner/banner.component';
 import { CategoryComponent } from '../category/category.component';
 import { FeaturedProductsComponent } from '../featured-products/featured-products.component';
+import { FacebookLoginComponent } from '../facebook-login/facebook-login.component';
 
 @Component({
   selector: 'app-home',
-  imports: [BannerComponent,CategoryComponent,FeaturedProductsComponent],
+  imports: [BannerComponent,CategoryComponent,FeaturedProductsComponent,FacebookLoginComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
